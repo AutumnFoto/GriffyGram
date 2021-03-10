9542060f1b84689310abbb8e5cea5f4ec872230d
Autumn Fotopoulos <acarte94@gmail.com>
2021-03-10 15:32:13 -0600

    finished navbar and footer

---
+++ src/scripts/nav/NavBar.js
@@ -0,0 +1,18 @@
+export const NavBar = () => {
+  return ` 
+    <nav class= "navigation">
+    <div class= "navigation_item navigation_icon">
+    <img src= "./images/pb.jpg" alt= "Griffygram icon"/>
+    </div>
+    <div class="navigation__item navigation__search">
+    <input type="text" id="postSearch" placeholder="Search posts..." />
+</div>
+<div class="navigation__item navigation__message">
+    <img id="directMessageIcon" src="./images/fountain-pen-_2_.jpg" alt="Direct message" />
+</div>
+<div class="navigation__item navigation__logout">
+    <button id="logout" class="fakeLink">Logout</button>
+</div>
+</nav>
+    `;
+};
