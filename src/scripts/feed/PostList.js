b7b95f2c248769fb88df7b7ddb6ade01dbc98021
Autumn Fotopoulos <acarte94@gmail.com>
2021-03-12 09:13:01 -0600

    getting form to clear

--- src/scripts/feed/PostList.js
+++ src/scripts/feed/PostList.js
@@ -10,3 +10,4 @@ export const PostList = (allPosts) => {
 		return postHTML;
 	
 }
+
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,18 @@
 
-import { getPosts } from "./data/DataManager.js"
+import { getPosts, getUsers, usePostCollection, getLoggedInUser, createPost } from "./data/DataManager.js"
 import { PostList } from "./feed/PostList.js"
 import { NavBar } from "./nav/NavBar.js"
 import { Footer } from "./nav/Footer.js"
+import { clearForm, PostEntry } from "./feed/PostEntry.js"
 
 
 
 const applicationElement = document.querySelector(".giffygram");
 const footerElement = document.querySelector("footer");
 
-footerElement.addEventListener("mousemove", event => {
-	console.log("moving now", event.x)
-})
+// footerElement.addEventListener("mousemove", event => {
+// 	console.log("moving now", event.x)
+// })
 
 applicationElement.addEventListener("click", (event) => {
 	console.log('click edit');
@@ -29,9 +30,44 @@ applicationElement.addEventListener("change", event => {
 	  const yearAsNumber =  parseInt(event.target.value)
   
 	  console.log(`User wants to see posts since ${yearAsNumber}`)
+	  showFilteredPosts(yearAsNumber);
 	}
   })
 
+  applicationElement.addEventListener("click", event => {
+	  if(event.target.id === "newPost__cancel"){
+
+	  }
+  })
+
+  applicationElement.addEventListener("click", event => {
+	  event.preventDefault();
+	  if(event.target.id === "newPost__submit"){
+		  const title = document.querySelector("input[name='postTitle']").value
+		  const url = document.querySelector("input[name='postUrl']").value
+		  const description = document.querySelector("textarea[name='postDescription']").value
+
+		  const postObject= {
+			  title: title, 
+			  imageUrl: url, 
+			  description: description,
+			  userId: getLoggedInUser().id,
+			  timestamp: Date.now(),
+			  get timestamp() {
+				  return this._timestamp
+			  },
+			  set timestamp(value) {
+				  this._timestamp = value
+			  },
+		  }
+		  clearForm();
+
+		  createPost(postObject)
+		  .then(response => {
+			  showPostList();
+		  })
+	  }
+  })
 
 const showPostList = () => {
 	const postElement = document.querySelector(".postList");
@@ -50,6 +86,7 @@ const showFooter = () => {
 	const footerElement = document.querySelector("footer");
 	footerElement.innerHTML = Footer();
 }
+
 
 const startGiffyGram = () => {
 	showNavBar();
