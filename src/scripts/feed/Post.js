e7abacc37717ff4ea98935b35cae4774c5607ca7
Autumn Fotopoulos <acarte94@gmail.com>
2021-03-23 18:42:55 -0500

    edit is working, delete is working but deleting everything going to ask for help in the morning

--- src/scripts/feed/Post.js
+++ src/scripts/feed/Post.js
@@ -1,12 +1,12 @@
 export const Post = (postObject) => {
   return `
-      <section class="post">
-        <header>
-            <h2 class="post__title">${postObject.title}</h2>
-        </header>
-        <img class="post__image" src="${postObject.imageURL}" alt="${postObject.description}"/>
-        <div><button id="edit--${postObject.id}--forfun">Edit</button></div>
-      </section>
-    `;
-};
-
+    <section class="post">
+      <header>
+          <h2 class="post__title">${postObject.title}</h2>
+      </header>
+      <img class="post__image" src="${postObject.imageURL}" alt="${postObject.description}"/>
+      <button id="edit__${postObject.id}">Edit</button>
+      <button id="delete__${postObject.id}">Delete</button>
+    </section>
+  `
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,93 +1,133 @@
 
-import { getPosts, getUsers, usePostCollection, getLoggedInUser, createPost } from "./data/DataManager.js"
-import { PostList } from "./feed/PostList.js"
-import { NavBar } from "./nav/NavBar.js"
-import { Footer } from "./nav/Footer.js"
-import { clearForm, PostEntry } from "./feed/PostEntry.js"
-
-
+import { getUsers, getPosts, usePostCollection, getLoggedInUser, createPost, deletePost, getSinglePost, updatePost } from "./data/DataManager.js";
+import { PostList } from "./feed/PostList.js";
+import { NavBar } from "./nav/NavBar.js";
+import { Footer } from "./nav/Footer.js";
+import { PostEntry } from "./feed/PostEntry.js";
+import { PostEdit } from "./feed/PostEdit.js";
+/**
+ * Main logic module for what should happen on initial page load for Giffygram
+ */
 
 const applicationElement = document.querySelector(".giffygram");
 const footerElement = document.querySelector("footer");
 
-// footerElement.addEventListener("mousemove", event => {
-// 	console.log("moving now", event.x)
-// })
 
-applicationElement.addEventListener("click", (event) => {
-	console.log('click edit');
-	
+applicationElement.addEventListener("click", event => {
+	event.preventDefault();
 	if (event.target.id.startsWith("edit")) {
-		console.log("post clicked", event.target.id.split("--"));
-		console.log("zero index value", event.target.id.split("--")[0]);
-		console.log("one index value, the id is", event.target.id.split("--")[1]);
+		const postId = event.target.id.split("__")[1];
+		getSinglePost(postId)
+			.then(response => {
+				showEdit(response);
+			})
 	}
 })
 
 applicationElement.addEventListener("change", event => {
-	// console.log('event obj', event);
 	if (event.target.id === "yearSelection") {
-	  const yearAsNumber =  parseInt(event.target.value)
-  
-	  console.log(`User wants to see posts since ${yearAsNumber}`)
-	  showFilteredPosts(yearAsNumber);
+		const yearAsNumber = parseInt(event.target.value)
+		console.log(`User wants to see posts since ${yearAsNumber}`)
+		//invoke a filter function passing the year as an argument
+		showFilteredPosts(yearAsNumber);
 	}
-  })
+})
 
-  applicationElement.addEventListener("click", event => {
-	  if(event.target.id === "newPost__cancel"){
+applicationElement.addEventListener("click", event => {
+	if (event.target.id === "newPost__cancel") {
+		//clear the input fields
+	}
+})
 
-	  }
-  })
+applicationElement.addEventListener("click", event => {
+	event.preventDefault();
+	if (event.target.id === "newPost__submit") {
+		//collect the input values into an object to post to the DB
+		const title = document.querySelector("input[name='postTitle']")
+		const url = document.querySelector("input[name='postURL']").value
+		const description = document.querySelector("textarea[name='postDescription']").value
+		//we have not created a user yet - for now, we will hard code `1`.
+		//we can add the current time as well
+		const postObject = {
+			title: title.value,
+			imageURL: url,
+			description: description,
+			userId: getLoggedInUser().id,
+			timestamp: Date.now()
+		}
 
-  applicationElement.addEventListener("click", event => {
-	  event.preventDefault();
-	  if(event.target.id === "newPost__submit"){
-		  const title = document.querySelector("input[name='postTitle']").value
-		  const url = document.querySelector("input[name='postUrl']").value
-		  const description = document.querySelector("textarea[name='postDescription']").value
+		// be sure to import from the DataManager
+		createPost(postObject)
+			.then(response => {
+				console.log("what is the new post response", response)
+				showPostList();
+			})
+	}
+})
 
-		  const postObject= {
-			  title: title, 
-			  imageUrl: url, 
-			  description: description,
-			  userId: getLoggedInUser().id,
-			  timestamp: Date.now(),
-			  get timestamp() {
-				  return this._timestamp
-			  },
-			  set timestamp(value) {
-				  this._timestamp = value
-			  },
-		  }
-		  clearForm();
+applicationElement.addEventListener("click", event => {
+	event.preventDefault();
+	if (event.target.id.startsWith("delete")) {
+		console.log("split", event.target.id.split("__"))
+		const postId = event.target.id.split("__")[1];
+		console.log("test", event.target.id.split("__")[1])
+		deletePost(postId)
+			.then(response => {
+				showPostList();
+			})
+	}
+})
 
-		  createPost(postObject)
-		  .then(response => {
-			  showPostList();
-		  })
-	  }
-  })
+applicationElement.addEventListener("click", event => {
+	event.preventDefault();
+	if (event.target.id.startsWith("updatePost")) {
+		const postId = event.target.id.split("__")[1];
+		//collect all the details into an object
+		const title = document.querySelector("input[name='postTitle']").value
+		const url = document.querySelector("input[name='postURL']").value
+		const description = document.querySelector("textarea[name='postDescription']").value
+		const timestamp = document.querySelector("input[name='postTime']").value
 
-  const showFilteredPosts = (year) => {
+		const postObject = {
+			title: title,
+			imageURL: url,
+			description: description,
+			userId: getLoggedInUser().id,
+			timestamp: parseInt(timestamp),
+			id: parseInt(postId)
+		}
+
+		showPostEntry();
+
+		updatePost(postObject)
+			.then(response => {
+				showPostList();
+			})
+	}
+})
+
+const showEdit = (postObj) => {
+	const entryElement = document.querySelector(".entryForm");
+	entryElement.innerHTML = PostEdit(postObj);
+}
+
+const showFilteredPosts = (year) => {
 	//get a copy of the post collection
 	const epoch = Date.parse(`01/01/${year}`);
 	//filter the data
 	const filteredData = usePostCollection().filter(singlePost => {
-	  if (singlePost.timestamp >= epoch) {
-		return singlePost
-	  }
+		if (singlePost.timestamp >= epoch) {
+			return singlePost
+		}
 	})
 	const postElement = document.querySelector(".postList");
 	postElement.innerHTML = PostList(filteredData);
-  }
-
-
+}
 
 const showPostList = () => {
 	const postElement = document.querySelector(".postList");
 	getPosts().then((allPosts) => {
-		postElement.innerHTML = PostList(allPosts);
+		postElement.innerHTML = PostList(allPosts.reverse());
 	})
 }
 
@@ -102,18 +142,22 @@ const showFooter = () => {
 	footerElement.innerHTML = Footer();
 }
 
-const showPostEntry = () => { 
+const showPostEntry = () => {
 	//Get a reference to the location on the DOM where the nav will display
 	const entryElement = document.querySelector(".entryForm");
 	entryElement.innerHTML = PostEntry();
-  }
+}
 
-
+/*
+	This function performs one, specific task.
+	1. Can you explain what that task is?
+	2. Are you defining the function here or invoking it?
+*/
 const startGiffyGram = () => {
 	showNavBar();
 	showPostEntry()
 	showPostList();
 	showFooter();
 }
-
+// Are you defining the function here or invoking it?
 startGiffyGram();
